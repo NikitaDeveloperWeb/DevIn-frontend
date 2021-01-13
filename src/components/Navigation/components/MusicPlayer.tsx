9ca34f1fc149e61a4b234c1dae764e8fafdc40cc
Nikita <rusakdeveloepr@gmail.com>
2021-01-13 15:52:23 +0300

    music player creating

--- src/components/Navigation/components/MusicPlayer.tsx
+++ src/components/Navigation/components/MusicPlayer.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
 function MusicPlayer() {
   return (
-    <div>
-      <audio controls>
-        <source src="../../../assets/music/exat.mp3" type="audio/mpeg" />
-        Your browser does not support the
-        <code>audio</code> element.
-      </audio>
+    <div className="music">
+      <div className="music__content">
+        <div className="music__content__preview">
+          <MusicNoteIcon />
+        </div>
+        <audio controls>
+          <source src="../../../assets/music/exat.mp3" type="audio/mpeg" />
+          Your browser does not support the
+          <code>audio</code> element.
+        </audio>
+      </div>
+      <div className="music__playlist"></div>
     </div>
   );
 }
